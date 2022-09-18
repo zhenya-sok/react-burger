66d0a8b2e3451a81683b6b9f73f60d0eef885ccc
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-09-18 19:23:05 +0500

    компонент ingredients-group

---
+++ src/components/ingredients-group/ingredients-group.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './ingredients-group.module.scss';
+import Ingredient from '../ingredient/ingredient';
+import PropTypes from 'prop-types';
+import {ingredientPropTypes} from '../../utils/ingredientPropType';
+
+const IngredientsGroup = ({ingredients, type, showDetails}) => {
+
+    const bunsList = ingredients && ingredients.filter((ingredient) => ingredient.type === "bun");
+    const saucesList = ingredients && ingredients.filter((ingredient) => ingredient.type === "sauce");
+    const mainList = ingredients && ingredients.filter((ingredient) => ingredient.type === "main");
+
+    return (
+        <div className={`${styles.ingredientsBlock} pl-4 pr-4 pt-6 pb-2`}>
+            {type === "bun" && <Ingredient key={ingredients._id} showDetails={showDetails} ingredientData={bunsList} />}
+            {type === "sauce" && <Ingredient key={ingredients._id} showDetails={showDetails} ingredientData={saucesList} />}
+            {type === "main" && <Ingredient key={ingredients._id} showDetails={showDetails} ingredientData={mainList} />}
+        </div>
+    )
+}
+
+IngredientsGroup.propTypes = {
+    ingredients: PropTypes.arrayOf(ingredientPropTypes.isRequired).isRequired,
+    type: PropTypes.string.isRequired,
+    showDetails: PropTypes.func.isRequired,
+}
+
+export default IngredientsGroup;
