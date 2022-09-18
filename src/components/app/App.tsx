a2fe875a2bfa2ad976f22dd6fa2766e68ee748db
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-09-18 19:16:46 +0500

    преобразование компонента app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,41 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import './App.scss';
+import styles from './App.module.scss';
 import '@ya.praktikum/react-developer-burger-ui-components';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
-
-// Не хватило времени на реализацию jsx логики :(
+import { loadIngredients } from '../../utils/api';
 
 function App() {
-  const [state, setState] = useState({
-    data: [],
-  })
-
-  const ingredientsListUrl = "https://norma.nomoreparties.space/api/ingredients";
-
-  const getIngredients = () => {
-
-    setState({ ...state});
-    fetch(ingredientsListUrl)
-      .then(res => res.json())
-      .then(data => setState({ ...state, data}))
-      .catch(e => {
-        setState({ ...state});
-      });
-  };
+  const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
-    getIngredients();
+    loadIngredients()
+      .then(setIngredients)
+      .catch(err => {
+        alert("Произошла ошибка при загрузке данных.")
+      });
   }, [])
 
   return (
-    <div className="App">
+    <div className={styles.app}>
       <div className="appWrapper">
         <AppHeader />
-        <main className="mainWrapper">
-          <BurgerIngredients ingredients={state.data}/>
-          <BurgerConstructor />
+        <main className={styles.mainWrapper}>
+          <BurgerIngredients ingredients={ingredients} />
+          <BurgerConstructor ingredients={ingredients} />
         </main>
       </div>
     </div>
