ef7b77ea14d34bde6925429b0a5f67e9108d90cc
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-09-18 19:24:41 +0500

    компонент burger-ingredients

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import styles from './burger-ingredients.module.scss';
-import { Tab, CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
-import Ingredient from './ingredient/ingredient';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import IngredientsGroup from '../ingredients-group/ingredients-group';
+import IngredientDetail from '../ingredient-details/ingredient-details';
+import Modal from '../modal/modal';
+import PropTypes from 'prop-types';
+import {ingredientPropTypes} from '../../utils/ingredientPropType';
 
-const BurgerIngredients = (props) => {
-    const [current, setCurrent] = React.useState('Булки')
+const BurgerIngredients = ({ingredients}) => {
+    const [current, setCurrent] = React.useState('Булки');
+    const [detailsVisible, setDetailsVisible] = React.useState(false);
+    const [currentItem, setCurrentItem] = React.useState(null);
+
+    const showIngredientDetails = (item) => {
+        setCurrentItem(item);
+        setDetailsVisible(true);
+    };
+    
+    const closeIngredientDetails = () => {
+        setDetailsVisible(false);
+    };
 
     return (
-       <section className={`${styles.ingredientsWrapper} mr-10`}>
+       <>
+        <section className={`${styles.ingredientsWrapper} mr-10`}>
             <h1 className="text text_type_main-large mt-10 mb-5`">Соберите бургер</h1>
 
             <div className={`${styles.ingredientsTab} mt-5`}>
@@ -16,75 +32,31 @@ const BurgerIngredients = (props) => {
                 <Tab value="Начинки" active={current === 'Начинки'} onClick={setCurrent}>Начинки</Tab>
             </div>
 
-            {/* Данный фрагмент нужно реализовать с помощью списоков и выделить новый компонент Ingredient. На скорую руку пришлось пока что так реализовать :( */}
-
-            <div className={`${styles.ingredientsList} pt-10`}>
-                <h2 className="text text_type_main-medium">Булки</h2>
-                <div className={`${styles.ingredientsBlock} pl-4 pr-4 pt-6 pb-10`}>
-                    {/* {props.success && props.data.map((ingredient, index) => <Ingredient key={index} data={ingredient} />)} */}
-
-                    <div className={`${styles.ingredientsItem} mr-6 pr-4 pl-4`}>
-                        <img src="" alt="изображение ингредиента" />
-                        <div className={`${styles.price} mt-1 mb-1`}>
-                            <span className="text text_type_digits-default">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <div className={`${styles.title} text text_type_main-default`}>Краторная булка N-200i</div>
-                        <Counter count={1} size="default" />
-                    </div>
-
-                    <div className={`${styles.ingredientsItem} mr-6 pr-4 pl-4`}>
-                        <img src="" alt="изображение ингредиента" />
-                        <div className={`${styles.price} mt-1 mb-1`}>
-                            <span className="text text_type_digits-default">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <div className={`${styles.title} text text_type_main-default`}>Флюоресцентная булка</div>
-                    </div>
-                </div>
-
-                <h2 className="text text_type_main-medium">Соусы</h2>
-
-                <div className={`${styles.ingredientsBlock} pl-4 pr-4 pt-6 pb-10`}>
-                    <div className={`${styles.ingredientsItem} mr-6 pr-4 pl-4 mb-8`}>
-                        <img src="" alt="изображение ингредиента" />
-                        <div className={`${styles.price} mt-1 mb-1`}>
-                            <span className="text text_type_digits-default">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <div className={`${styles.title} text text_type_main-default`}>Соус Spicy-X</div>
-                    </div>
-
-                    <div className={`${styles.ingredientsItem} mr-6 pr-4 pl-4 mb-8`}>
-                        <img src="" alt="изображение ингредиента" />
-                        <div className={`${styles.price} mt-1 mb-1`}>
-                            <span className="text text_type_digits-default">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <div className={`${styles.title} text text_type_main-default`}>Соус Spicy-X</div>
-                    </div>
-
-                    <div className={`${styles.ingredientsItem} mr-6 pr-4 pl-4 mb-8`}>
-                        <img src="" alt="изображение ингредиента" />
-                        <div className={`${styles.price} mt-1 mb-1`}>
-                            <span className="text text_type_digits-default">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <div className={`${styles.title} text text_type_main-default`}>Соус Spicy-X</div>
-                    </div>
-
-                    <div className={`${styles.ingredientsItem} mr-6 pr-4 pl-4 mb-8`}>
-                        <img src="" alt="изображение ингредиента" />
-                        <div className={`${styles.price} mt-1 mb-1`}>
-                            <span className="text text_type_digits-default">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <div className={`${styles.title} text text_type_main-default`}>Соус Spicy-X</div>
-                    </div>
-                </div>
-            </div>
-       </section>
+            <ul className={`${styles.ingredientsList} mt-10`}>
+                <li>
+                    <h3 className="text text_type_main-medium">Булки</h3>
+                    <IngredientsGroup ingredients={ingredients} type={"bun"} showDetails={showIngredientDetails} />
+                </li>
+                <li>
+                    <h3 className="text text_type_main-medium">Соусы</h3>
+                    <IngredientsGroup ingredients={ingredients} type={"sauce"} showDetails={showIngredientDetails} />
+                </li>
+               <li>
+                    <h3 className="text text_type_main-medium">Основное</h3>
+                    <IngredientsGroup ingredients={ingredients} type={"main"} showDetails={showIngredientDetails} />
+               </li>
+            </ul>
+        </section>
+        {detailsVisible && 
+        <Modal closeModal={closeIngredientDetails} titleText="Детали ингредиента">
+            <IngredientDetail ingredientInfo={currentItem}/>
+        </Modal>}
+       </>
     )
 }
+
+BurgerIngredients.propTypes = {
+    ingredients: PropTypes.arrayOf(ingredientPropTypes.isRequired).isRequired,
+};
 
 export default BurgerIngredients;
