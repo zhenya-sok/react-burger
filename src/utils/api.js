6c3f1ea5147cc9bb930ae37b01f0a58f8bd801bf
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-10-17 01:43:53 +0400

    Создание api запроса для получения номера заказа

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,3 +12,19 @@ export function loadIngredients() {
     if (data.success) return data.data;
   })
 };
+
+const orderDetailsUrl = "https://norma.nomoreparties.space/api/orders";
+
+export function loadOrderDetails(newOrder) {
+  return fetch(orderDetailsUrl, {
+    method: 'POST',
+    body: JSON.stringify(newOrder),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) return data;
+    })
+}
