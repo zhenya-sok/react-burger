295167fe8316c3f8829ed265a4f904d749a11a00
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-10-17 01:47:32 +0400

    Доработка компонента burger-ingredients

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import styles from './burger-ingredients.module.css';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import IngredientsGroup from '../ingredients-group/ingredients-group';
 import IngredientDetail from '../ingredient-details/ingredient-details';
 import Modal from '../modal/modal';
-import PropTypes from 'prop-types';
-import {ingredientPropTypes} from '../../utils/ingredientPropType';
+import { IngredientDataContext } from '../../services/ingredientDataContext';
 
-const BurgerIngredients = ({ingredients}) => {
-    const [current, setCurrent] = React.useState('Булки');
-    const [detailsVisible, setDetailsVisible] = React.useState(false);
-    const [currentItem, setCurrentItem] = React.useState(null);
+const BurgerIngredients = () => {
+    const { ingredientsCategories } = useContext(IngredientDataContext);
+    const [currentTab, setCurrentTab] = useState("Булки");
+    const [detailsVisible, setDetailsVisible] = useState(false);
+    const [currentItem, setCurrentItem] = useState(null);
 
     const showIngredientDetails = (item) => {
         setCurrentItem(item);
@@ -27,25 +27,28 @@ const BurgerIngredients = ({ingredients}) => {
             <h1 className="text text_type_main-large mt-10 mb-5`">Соберите бургер</h1>
 
             <div className={`${styles.ingredientsTab} mt-5`}>
-                <Tab value="Булки" active={current === 'Булки'} onClick={setCurrent}>Булки</Tab> 
-                <Tab value="Соусы" active={current === 'Соусы'} onClick={setCurrent}>Соусы</Tab>
-                <Tab value="Начинки" active={current === 'Начинки'} onClick={setCurrent}>Начинки</Tab>
+                <Tab value="Булки" active={currentTab === 'Булки'}>Булки</Tab> 
+                <Tab value="Соусы" active={currentTab === 'Соусы'}>Соусы</Tab>
+                <Tab value="Начинки" active={currentTab === 'Начинки'}>Начинки</Tab>
             </div>
 
-            <ul className={`${styles.ingredientsList} mt-10`}>
-                <li>
-                    <h3 className="text text_type_main-medium">Булки</h3>
-                    <IngredientsGroup ingredients={ingredients} type={"bun"} showDetails={showIngredientDetails} />
-                </li>
-                <li>
-                    <h3 className="text text_type_main-medium">Соусы</h3>
-                    <IngredientsGroup ingredients={ingredients} type={"sauce"} showDetails={showIngredientDetails} />
-                </li>
-               <li>
-                    <h3 className="text text_type_main-medium">Основное</h3>
-                    <IngredientsGroup ingredients={ingredients} type={"main"} showDetails={showIngredientDetails} />
-               </li>
-            </ul>
+            <div className={`${styles.ingredientsList} mt-10`}>
+                <IngredientsGroup
+                    title="Булки"
+                    ingredients={ingredientsCategories.bun}
+                    showDetails={showIngredientDetails}
+                />
+                <IngredientsGroup 
+                    title="Соусы"
+                    ingredients={ingredientsCategories.sauce} 
+                    showDetails={showIngredientDetails}
+                />
+                <IngredientsGroup
+                    title="Онсновное"
+                    ingredients={ingredientsCategories.main}
+                    showDetails={showIngredientDetails}
+                />
+            </div>
         </section>
         {detailsVisible && (
             <Modal closeModal={closeIngredientDetails} titleText="Детали ингредиента">
@@ -55,9 +58,5 @@ const BurgerIngredients = ({ingredients}) => {
        </>
     )
 }
-
-BurgerIngredients.propTypes = {
-    ingredients: PropTypes.arrayOf(ingredientPropTypes.isRequired).isRequired,
-};
 
 export default BurgerIngredients;
