33eaae401b159f204360796a3b2904cfbf93089f
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-10-24 21:41:55 +0400

    Редьюсер разбит на несколько редьюсеров

---
+++ src/services/reducers/constructorReducer.js
@@ -0,0 +1,30 @@
+import {
+    SELECT_INGREDIENT,
+    UPDATE_CONSTRUCTOR_LIST,
+}
+    from '../actions';
+
+const initialState = {
+    selectedIngredients: [],
+    currentItem: null,
+}
+
+export const constructorReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SELECT_INGREDIENT: {
+            return {
+                ...state,
+                selectedIngredients: action.payload
+            }
+        }
+       
+        case UPDATE_CONSTRUCTOR_LIST: {
+            return {
+                ...state,
+                selectedIngredients: action.payload
+            }
+        }
+    }
+
+    return state
+}
