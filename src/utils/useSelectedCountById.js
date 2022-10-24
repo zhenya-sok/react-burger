f309cee3a08f5beec0ba585493c8b1ab91388f25
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-10-24 21:42:31 +0400

    Удвоение счетчика булки

--- src/utils/useSelectedCountById.js
+++ src/utils/useSelectedCountById.js
@@ -1,11 +1,14 @@
 import { useSelector } from "react-redux";
 
 export function useSelectedCountById(ingredientId) {
-    const selectedIngredients = useSelector((store) => store.ingredientsReducer.selectedIngredients)
+    const selectedIngredients = useSelector((store) => store.constructorReducer.selectedIngredients)
 
     return selectedIngredients.reduce((result, currentIngredient) => {
         if (currentIngredient._id === ingredientId) {
             result++
+        }
+        if (currentIngredient.type === "bun") {
+            return result * 2
         }
 
         return result
