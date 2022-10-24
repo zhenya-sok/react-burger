fa620c14f0a6b2e926286b02e87c4d0742e74b36
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-10-24 21:40:36 +0400

    store вынесен в отдельный файл

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,7 @@ import './index.css';
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { rootReducer } from './services/reducers/ingredients';
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(rootReducer, enhancer);
+import { store } from './services/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
