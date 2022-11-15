4bf6af9fd49d241b7b59185882b568edd1544c49
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-11-15 22:50:50 +0500

    экшен и редьюсер для задач авторизации

---
+++ src/services/reducers/authReducer.js
@@ -0,0 +1,107 @@
+import {
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_ERROR,
+
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGIN_ERROR,
+
+    CHECK_AUTH_SESSION,
+    LOGOUT,
+    GET_USER,
+    UPDATE_USER,
+}
+    from '../actions/authActions';
+
+const initialState = {
+    user: undefined,
+    registerRequest: false,
+    registerError: false,
+    loginRequest: false,
+    loginError: false,
+    token: undefined,
+}
+
+export const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case REGISTER_USER_REQUEST: {
+            return {
+                ...state,
+                registerRequest: true,
+                registerError: false,
+            }
+        }
+        case REGISTER_USER_SUCCESS: {
+            return {
+                ...state,
+                registerError: false,
+                user: action.payload,
+                registerRequest: false,
+                token: action.payload.accessToken
+            }
+        }
+        case REGISTER_USER_ERROR: {
+            return {
+                ...state,
+                registerError: true,
+                registerRequest: false,
+            }
+        }
+
+        case LOGIN_REQUEST: {
+            return {
+                ...state,
+                loginRequest: true,
+                loginError: false,
+            }
+        }
+        case LOGIN_SUCCESS: {
+            return {
+                ...state,
+                loginError: false,
+                user: action.payload.user,
+                token: action.payload.accessToken,
+                loginRequest: false,
+            }
+        }
+        case LOGIN_ERROR: {
+            return {
+                ...state,
+                loginError: true,
+                loginRequest: false
+            }
+        }
+
+        case CHECK_AUTH_SESSION: {
+            return {
+                ...state,
+                token: action.payload
+            }
+        }
+
+        case LOGOUT: {
+            return {
+                ...state,
+                user: undefined,
+                token: undefined,
+            }
+        }
+
+        case GET_USER: {
+            return {
+                ...state,
+                user: action.payload,
+            }
+        }
+
+        case UPDATE_USER: {
+            return {
+                ...state,
+                user: action.payload,
+            }
+        }
+    }
+
+    return state
+}
