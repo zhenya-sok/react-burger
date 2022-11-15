484c61bac80f3b11053e76a60dbeb89daee2007a
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-11-15 22:45:37 +0500

    компонент login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,81 +1,84 @@
 import { Button, Input, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components';
-import React, { useCallback } from 'react';
+import React, { useEffect } from 'react';
 import styles from './login.module.css';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../services/actions/authActions';
 
 const Login = () => {
     const [emailValue, setEmailValue] = React.useState('')
-    const inputRef = React.useRef(null)
-    const onIconClick = () => {
-        setTimeout(() => inputRef.current.focus(), 0)
-        alert('Icon Click Callback')
-    }
 
     const [passwordValue, setPasswordValue] = React.useState('');
-    const onChange = e => {
+
+    const onPasswordChange = e => {
         setPasswordValue(e.target.value)
     }
 
     const history = useHistory();
+    const location = useLocation();
+    const dispatch = useDispatch();
 
-    const navigateToRegister = useCallback(
-        () => {
-            history.replace({ pathname: '/register' });
-        },
-        [history]
-    );
+    const isAuth = useSelector((state) => !!state.authReducer.token);
+    
+    useEffect(() => {
+        isAuth && history.replace({pathname: `${location.state}`});
+    }, [isAuth])
 
-    const navigateToForgotPassword = useCallback(
-        () => {
-            history.replace({ pathname: '/forgot-password' });
-        },
-        [history]
-    );
-
+    function handleSubmit(e) {
+        e.preventDefault();
+        dispatch(login({
+            email: emailValue,
+            password: passwordValue
+        }))
+    }
+    
     return (
         <section className={styles.loginWrapper}>
             <h1>Вход</h1>
 
-            <div className={`${styles.loginInput} mt-6 mb-6`}>
-                <Input
-                    type={'email'}
-                    placeholder={'E-mail'}
-                    onChange={e => setEmailValue(e.target.value)}
-                    value={emailValue}
-                    name={'name'}
-                    error={false}
-                    ref={inputRef}
-                    onIconClick={onIconClick}
-                    errorText={'Ошибка'}
-                    size={'default'}
-                    extraClass="ml-1"
-                />
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div className={`${styles.loginInput} mt-6 mb-6`}>
+                    <Input
+                        type={'email'}
+                        placeholder={'E-mail'}
+                        onChange={e => setEmailValue(e.target.value)}
+                        value={emailValue}
+                        name={'name'}
+                        error={false}
+                        errorText={'Ошибка'}
+                        size={'default'}
+                        extraClass="ml-1"
+                    />
+                </div>
 
-            <div className={`${styles.loginInput} mb-6`}>
-                <PasswordInput
-                    onChange={onChange}
-                    value={passwordValue}
-                    name={'password'}
-                    extraClass="mb-2"
-                />
-            </div>
+                <div className={`${styles.loginInput} mb-6`}>
+                    <PasswordInput
+                        onChange={onPasswordChange}
+                        value={passwordValue}
+                        name={'password'}
+                        extraClass="mb-2"
+                    />
+                </div>
 
-            <Button type="primary" size="medium">Войти</Button>
+                <Button htmlType="submit" 
+                    type="primary" 
+                    size="medium" 
+                    disabled={!emailValue || !passwordValue}>
+                    Войти
+                </Button>
+            </form>
 
-            <div className="mb-4">
-                <span className="text text_type_main-default text_color_inactive">Вы новый пользователь?</span>
-                
-                <span className={styles.registerBtn}>
-                    <Button type="secondary" size="large" onClick={() => navigateToRegister()}>
-                        Зарегестрироваться
-                    </Button>
-                </span>
-
+            <div className="mt-20 mb-4">
+                <span className="text text_type_main-default text_color_inactive">Вы новый пользователь? </span>
+                <Link to='/register' className={styles.navigateBtn}>
+                    Зарегестрироваться
+                </Link>
             </div>
             <div>
-                <span className="text text_type_main-default text_color_inactive">Забыли пароль?</span>
-                <Button type="secondary" size="large" onClick={() => navigateToForgotPassword()}>Восстановить пароль</Button>
+                <span className="text text_type_main-default text_color_inactive">Забыли пароль? </span>
+                <Link to="/forgot-password" className={styles.navigateBtn}>
+                    Восстановить пароль
+                </Link>
             </div>
         </section>
     )
