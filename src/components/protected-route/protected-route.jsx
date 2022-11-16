b38f0095409a668f39700251ed848ec056ebc9f1
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-11-16 16:50:06 +0500

    улучшение protected-route

--- src/components/protected-route/protected-route.jsx
+++ src/components/protected-route/protected-route.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = ({ redirectTo, path, ...props }) => {
+const ProtectedRoute = ({ onlyForAuth, children, ...rest }) => {
     const isAuth = useSelector((state) => !!state.authReducer.token);
+    const location = useLocation();
 
-    if (isAuth) {
-        return <Route {...props} />
-    } else {
-        return <Redirect to={{
-                                pathname: `${redirectTo}`,
-                                state: `${path}`
-                            }} />
+    if (!onlyForAuth && isAuth) {
+        const { from } = location.state || { from: { pathname: "/" } };
+
+        return (
+            <Route {...rest}>
+                <Redirect to={from} />
+            </Route>
+        );
     }
-}
+
+    if (onlyForAuth && !isAuth) {
+        return (
+            <Route {...rest}>
+                <Redirect to={{ pathname: "/login", state: { from: location } }} />
+            </Route>
+        );
+    }
+
+    return <Route {...rest}>{children}</Route>;
+};
 
 export default ProtectedRoute;
