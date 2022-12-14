d52c4b969b4eeed28c2583c7e3b988210382bb76
Evgeniy Sokolov <sockolovzhenya@yandex.ru>
2022-12-14 20:59:03 +0500

    тесты для authReducer

--- src/services/reducers/authReducer/authReducer.test.js
+++ src/services/reducers/authReducer/authReducer.test.js
@@ -1,18 +1,15 @@
 import { initialState, authReducer } from './authReducer';
-import { REGISTER_USER_REQUEST } from '../../actions/authActions';
-import { CHECK_AUTH_SESSION, GET_USER, LOGIN_ERROR, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT, REGISTER_USER_ERROR, REGISTER_USER_SUCCESS, UPDATE_USER } from '../../constants';
+import { REGISTER_USER_REQUEST, CHECK_AUTH_SESSION, GET_USER, LOGIN_ERROR, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT, REGISTER_USER_ERROR, REGISTER_USER_SUCCESS, UPDATE_USER } from '../../constants';
 
 describe('auth reducer', () => {
-    it('should return the initial state', () => {
+    it.only('should return the initial state', () => {
         expect(authReducer(undefined, {})).toEqual(initialState)
     })
 
-    it('REGISTER_USER_REQUEST', () => {
+    it.only('REGISTER_USER_REQUEST', () => {
         expect(
-            authReducer(undefined, {
-                type: REGISTER_USER_REQUEST,
-                registerRequest: true,
-                registerError: false,
+            authReducer({...initialState, registerRequest: false, registerError: true}, {
+                type: REGISTER_USER_REQUEST
             })
         ).toEqual(
             {
@@ -23,32 +20,38 @@ describe('auth reducer', () => {
         )
     });
 
-    it('REGISTER_USER_SUCCESS', () => {
+    it.only('REGISTER_USER_SUCCESS', () => {
         expect(
-            authReducer(undefined, {
+            authReducer(
+            {...initialState,
+                registerError: true,
+                user: undefined,
+                registerRequest: true,
+                token: undefined
+            },
+            {
                 type: REGISTER_USER_SUCCESS,
-                registerError: false,
-                user: { type: REGISTER_USER_SUCCESS, payload: { res: ['testUser'] } },
-                registerRequest: false,
-                token: { type: REGISTER_USER_SUCCESS, payload: { accessToken: ['testAccessToken'] } },
+                payload: {
+                    accessToken: 'testAccessToken'
+                }
             })
         ).toEqual(
             {
                 ...initialState,
                 registerError: false,
-                user: { type: REGISTER_USER_SUCCESS, payload: { res: ['testUser'] } },
+                user: {
+                  accessToken: 'testAccessToken'
+                },
                 registerRequest: false,
-                token: { type: REGISTER_USER_SUCCESS, payload: { accessToken: ['testAccessToken'] } },
+                token: 'testAccessToken'
             }
         )
     });
 
-    it('REGISTER_USER_ERROR', () => {
+    it.only('REGISTER_USER_ERROR', () => {
         expect(
-            authReducer(undefined, {
+            authReducer({...initialState, registerRequest: true, registerError: false}, {
                 type: REGISTER_USER_ERROR,
-                registerError: true,
-                registerRequest: false,
             })
         ).toEqual(
             {
@@ -59,12 +62,10 @@ describe('auth reducer', () => {
         )
     });
 
-    it('LOGIN_REQUEST', () => {
+    it.only('LOGIN_REQUEST', () => {
         expect(
-            authReducer(undefined, {
+            authReducer({...initialState, loginRequest: false, loginError: true}, {
                 type: LOGIN_REQUEST,
-                loginRequest: true,
-                loginError: false,
             })
         ).toEqual(
             {
@@ -75,32 +76,43 @@ describe('auth reducer', () => {
         )
     });
 
-    it('LOGIN_SUCCESS', () => {
+    it.only('LOGIN_SUCCESS', () => {
         expect(
-            authReducer(undefined, {
+            authReducer({
+                ...initialState,
+                loginError: true,
+                user: undefined,
+                accessToken: undefined,
+                loginRequest: true,
+            },
+            {
                 type: LOGIN_SUCCESS,
-                loginError: false,
-                user: { type: REGISTER_USER_SUCCESS, payload: { res: ['testUser'] } },
-                token: { type: REGISTER_USER_SUCCESS, payload: { accessToken: ['testAccessToken'] } },
-                loginRequest: false,
+                payload: {
+                    accessToken: 'testAccessToken',
+                    user: {
+                        email: "email",
+                        name: "name"
+                    },
+                },
             })
         ).toEqual(
             {
                 ...initialState,
                 loginError: false,
-                user: { type: REGISTER_USER_SUCCESS, payload: { res: ['testUser'] } },
-                token: { type: REGISTER_USER_SUCCESS, payload: { accessToken: ['testAccessToken'] } },
+                user: {
+                    email: "email",
+                    name: "name"
+                },
+                token: 'testAccessToken',
                 loginRequest: false,
             }
         )
     });
 
-    it('LOGIN_ERROR', () => {
+    it.only('LOGIN_ERROR', () => {
         expect(
-            authReducer(undefined, {
+            authReducer({...initialState, loginError: false, loginRequest: true}, {
                 type: LOGIN_ERROR,
-                loginError: true,
-                loginRequest: false
             })
         ).toEqual(
             {
@@ -111,21 +123,25 @@ describe('auth reducer', () => {
         )
     });
 
-    it('CHECK_AUTH_SESSION', () => {
+    it.only('CHECK_AUTH_SESSION', () => {
         expect(
-            authReducer(undefined, {
+            authReducer({...initialState, token: undefined}, {
                 type: CHECK_AUTH_SESSION,
-                token: { type: REGISTER_USER_SUCCESS, payload: { accessToken: ['testAccessToken'] } },
+                payload: {
+                    accessToken: 'testAccessToken'
+                }
             })
         ).toEqual(
             {
                 ...initialState,
-                token: { type: REGISTER_USER_SUCCESS, payload: { accessToken: ['testAccessToken'] } },
+                token: {
+                    accessToken: 'testAccessToken'
+                }
             }
         )
     });
 
-    it('LOGOUT', () => {
+    it.only('LOGOUT', () => {
         expect(
             authReducer(undefined, {
                 type: LOGOUT,
@@ -141,30 +157,42 @@ describe('auth reducer', () => {
         )
     });
 
-    it('GET_USER', () => {
+    it.only('GET_USER', () => {
         expect(
-            authReducer(undefined, {
+            authReducer({...initialState, user: undefined}, {
                 type: GET_USER,
-                user: { type: REGISTER_USER_SUCCESS, payload: { res: ['testUser'] } },
+                payload: {
+                    email: "email",
+                    name: "name"
+                }
             })
         ).toEqual(
             {
                 ...initialState,
-                user: { type: REGISTER_USER_SUCCESS, payload: { res: ['testUser'] } },
+                user: {
+                    email: "email",
+                    name: "name"
+                }
             }
         )
     });
 
-    it('UPDATE_USER', () => {
+    it.only('UPDATE_USER', () => {
         expect(
-            authReducer(undefined, {
+            authReducer({...initialState, user: undefined}, {
                 type: UPDATE_USER,
-                user: { type: REGISTER_USER_SUCCESS, payload: { res: ['testUser'] } },
+                payload: {
+                    email: "email",
+                    name: "name"
+                }
             })
         ).toEqual(
             {
                 ...initialState,
-                user: { type: REGISTER_USER_SUCCESS, payload: { res: ['testUser'] } },
+                user: {
+                    email: "email",
+                    name: "name"
+                }
             }
         )
     });
